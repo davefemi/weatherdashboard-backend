efaf03b5b15ea35f4b22102e9e3b52e5f5d20e2d
davefemi <david.ojo@live.com>
2025-03-20 19:30:14 +0100

    Main version of project

---
+++ src/test/java/nl/davefemi/weatherdashboard/WeatherdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.davefemi.weatherdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
